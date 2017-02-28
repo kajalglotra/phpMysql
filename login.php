3f08610a72134d4063f160feb8c103368bbe10a8
kajalglotra <bulbulkjl@gmail.com>
2017-02-28 15:19:29 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,37 @@
 <?php
    include("config.php");
    session_start();
-   $error='';
+ $error='';
+  if(isset($_SESSION['login_user'])){
+   header("location:welcome.php");
+  } 
+  if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password']) ) {
+    // username and password sent from form
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM admin_account WHERE username = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-      
-      if($count == 1) {
-         echo($myusername);
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+   
+    $sql = "SELECT id FROM admin_account WHERE username = '$myusername' and password = '$mypassword'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+   
+    $count = mysqli_num_rows($result);
+   
+    // If result matched $myusername and $mypassword, table row must be 1 row
+   
+    if($count == 1) {
+       echo($myusername);
+       $_SESSION['login_user'] = $myusername;
+     
+       header("location: welcome.php");
       }
-   }
+     else {
+       $error = "Your Login Name or Password is invalid";
+    }
+ }
+  elseif(!isset($_POST['username']) && empty($_POST['username']) && !isset($_POST['password']) && empty($_POST['password']) ){
+    $error = "Your Login Name or Password is invalid";
+  }
 ?>
 <html>
    
