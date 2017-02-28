1ce881d3a1771d388afbe8e20ab0c758042ccbf0
satyametch <satya@excellencetechnologies.in>
2017-02-28 13:15:05 +0530

    phplogin

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'excelarf');
+   define('DB_PASSWORD', '**T0y*6z8e0c');
+   define('DB_DATABASE', 'excelarf_phpMysql');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
